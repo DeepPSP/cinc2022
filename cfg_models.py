5bfc0e86998377470b7132743df265b49cd1ff68
WEN Hao <wenh06@gmail.com>
2022-03-25 00:17:02 +0800

    add cfg_models

---
+++ cfg_models.py
@@ -0,0 +1,52 @@
+"""
+"""
+
+
+from copy import deepcopy
+from itertools import repeat
+
+from torch_ecg.cfg import CFG
+from torch_ecg.model_configs import (
+    # cnn bankbone
+    vgg_block_basic, vgg_block_mish, vgg_block_swish,
+    vgg16, vgg16_leadwise,
+    resnet_block_basic, resnet_bottle_neck_B, resnet_bottle_neck_D,
+    resnet_block_basic_se, resnet_block_basic_gc,
+    resnet_bottle_neck_se, resnet_bottle_neck_gc,
+    resnet_nature_comm, resnet_nature_comm_se, resnet_nature_comm_gc,
+    resnet_nature_comm_bottle_neck, resnet_nature_comm_bottle_neck_se,
+    resnetN, resnetNB, resnetNS, resnetNBS,
+    tresnetF, tresnetP, tresnetN, tresnetS, tresnetM,
+    multi_scopic_block,
+    multi_scopic, multi_scopic_leadwise,
+    densenet_leadwise,
+    xception_leadwise,
+    # lstm
+    lstm,
+    attention,
+    # mlp
+    linear,
+    # attn
+    non_local,
+    squeeze_excitation,
+    global_context,
+    # the whole model config
+    ECG_CRNN_CONFIG,
+    ECG_SEQ_LAB_NET_CONFIG,
+    ECG_UNET_VANILLA_CONFIG,
+)
+
+
+__all__ = [
+    "ModelArchCfg",
+]
+
+
+# detailed configs for 12-lead, 6-lead, 4-lead, 3-lead, 2-lead models
+# mostly follow from torch_ecg.torch_ecg.model_configs.ecg_crnn
+ModelArchCfg = CFG()
+
+
+ModelArchCfg.classification = deepcopy(ECG_CRNN_CONFIG)
+
+ModelArchCfg.segmentation = deepcopy(ECG_SEQ_LAB_NET_CONFIG)
