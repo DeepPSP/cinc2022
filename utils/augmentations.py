68369d98f342d800d00c1b2ce616a065cd8d0d4e
WEN Hao <wenh06@gmail.com>
2022-03-28 22:34:51 +0800

    update augmenters

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -2,4 +2,25 @@
 Audio data augmentation using `torch_audiomentations`
 """
 
+from typing import Sequence, NoReturn
+
 import torch_audiomentations as TA
+from torch_audiomentations.core.transforms_interface import BaseWaveformTransform
+
+
+class Augmenter(TA.SomeOf):
+    """
+
+    Audio data augmenters
+
+    """
+
+    def __init__(
+        self,
+        transforms: Sequence[BaseWaveformTransform],
+        p: float = 1.0,
+    ) -> NoReturn:
+        """
+
+        """
+        super().__init__((1, None), transforms, p=p)
