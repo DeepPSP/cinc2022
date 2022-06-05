c084f042a2d30f15cb0ed7c40891e54c3de47aa2
WEN Hao <wenh06@gmail.com>
2022-06-05 17:53:28 +0800

    move DataCollatorForWav2Vec2Pretraining to a separate file

--- wav2vec2_hf/pretraining.py
+++ wav2vec2_hf/pretraining.py
@@ -4,8 +4,7 @@ modified from:
 https://github.com/huggingface/transformers/blob/main/examples/pytorch/speech-pretraining/run_wav2vec2_pretraining_no_trainer.py
 """
 
-from dataclasses import dataclass
-from typing import Dict, List, Tuple, Optional, Union, Any, NoReturn
+from typing import Dict, List, Tuple, Optional, Any, NoReturn
 
 import torch
 from torch import nn
@@ -22,99 +21,14 @@ from transformers import (  # noqa: F401
     is_wandb_available,
     set_seed,
 )
-from transformers.models.wav2vec2.modeling_wav2vec2 import (  # noqa: F401
-    _compute_mask_indices,
-    _sample_negative_indices,
-)
 from torch_ecg.components.trainer import BaseTrainer
 
 from .pretraining_cfg import PreTrainCfg, PreTrainModelCfg  # noqa: F401
 
 
-@dataclass
-class DataCollatorForWav2Vec2Pretraining:
-    """
-    Data collator that will dynamically pad the inputs received and prepare masked indices
-    for self-supervised pretraining.
-    Args:
-        model (:class:`~transformers.Wav2Vec2ForPreTraining`):
-            The Wav2Vec2 model used for pretraining. The data collator needs to have access
-            to config and ``_get_feat_extract_output_lengths`` function for correct padding.
-        feature_extractor (:class:`~transformers.Wav2Vec2FeatureExtractor`):
-            The processor used for proccessing the data.
-        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-              sequence if provided).
-            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
-              maximum acceptable input length for the model if that argument is not provided.
-            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
-              different lengths).
-        max_length (:obj:`int`, `optional`):
-            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
-        pad_to_multiple_of (:obj:`int`, `optional`):
-            If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-    """
-
-    model: Wav2Vec2ForPreTraining
-    feature_extractor: Wav2Vec2FeatureExtractor
-    padding: Union[bool, str] = "longest"
-    pad_to_multiple_of: Optional[int] = None
-
-    def __call__(
-        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
-    ) -> Dict[str, torch.Tensor]:
-        # reformat list to dict and set to pytorch format
-        batch = self.feature_extractor.pad(
-            features,
-            padding=self.padding,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-        )
-
-        device = batch["input_values"].device
-        batch_size = batch["input_values"].shape[0]
-
-        mask_indices_seq_length = self.model._get_feat_extract_output_lengths(
-            batch["input_values"].shape[-1]
-        )
-        # make sure masked sequence length is a Python scalar
-        mask_indices_seq_length = int(mask_indices_seq_length)
-
-        # make sure that no loss is computed on padded inputs
-        if batch.get("attention_mask") is not None:
-            # compute real output lengths according to convolution formula
-            batch["sub_attention_mask"] = self.model._get_feature_vector_attention_mask(
-                mask_indices_seq_length, batch["attention_mask"]
-            )
-
-        features_shape = (batch_size, mask_indices_seq_length)
-
-        # sample randomly masked indices
-        mask_time_indices = _compute_mask_indices(
-            features_shape,
-            self.model.config.mask_time_prob,
-            self.model.config.mask_time_length,
-            attention_mask=batch.get("sub_attention_mask"),
-        )
-
-        # sample negative indices
-        sampled_negative_indices = _sample_negative_indices(
-            features_shape,
-            self.model.config.num_negatives,
-            mask_time_indices=mask_time_indices,
-        )
-        batch["mask_time_indices"] = torch.tensor(
-            mask_time_indices, dtype=torch.long, device=device
-        )
-        batch["sampled_negative_indices"] = torch.tensor(
-            sampled_negative_indices, dtype=torch.long, device=device
-        )
-
-        return batch
+__all__ = [
+    "Wav2Vec2PreTrainingTrainer",
+]
 
 
 def multiply_grads(params, c):
