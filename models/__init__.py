ba6b7bd9d2099571c007db09b425b86f9bbaacc5
WEN Hao <wenh06@gmail.com>
2022-08-08 18:30:41 +0800

    add new experiment results

--- models/__init__.py
+++ models/__init__.py
@@ -7,6 +7,7 @@ Models, including:
 from .crnn import CRNN_CINC2022
 from .wav2vec2 import Wav2Vec2_CINC2022, HFWav2Vec2_CINC2022
 from .seg import SEQ_LAB_NET_CINC2022, UNET_CINC2022
+from .model_ml import OutComeClassifier_CINC2022
 
 
 __all__ = [
@@ -15,4 +16,5 @@ __all__ = [
     "HFWav2Vec2_CINC2022",
     "SEQ_LAB_NET_CINC2022",
     "UNET_CINC2022",
+    "OutComeClassifier_CINC2022",
 ]
