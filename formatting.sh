686ba88d30f85c8bd1802de055c3d4e238a07e17
WEN Hao <wenh06@gmail.com>
2022-08-11 17:05:52 +0800

    add black formating of jupyter notebooks

--- formatting.sh
+++ formatting.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-black . --extend-exclude .ipynb -v --exclude="/build|dist|official_baseline_classifier|official_scoring_metric/|helper_code\.py|run_model\.py|train_model\.py|evaluate_model\.py"
+black . -v --exclude="/build|dist|official_baseline_classifier|official_scoring_metric/|helper_code\.py|run_model\.py|train_model\.py|evaluate_model\.py"
 flake8 . --count --ignore="E501 W503 E203 F841 E402" --show-source --statistics --exclude=./.*,build,dist,official*,helper_code.py,run_model.py,train_model.py,evaluate_model.py,*.ipynb
