e5d60c536f1361f050e500da1c3cb1f5d4c00ab3
WEN Hao <wenh06@gmail.com>
2022-09-15 10:48:22 +0800

    update utils/_final_results.py

--- utils/_final_results.py
+++ utils/_final_results.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from datetime import datetime
 from pathlib import Path
 from typing import Union, NoReturn, Dict
 
@@ -71,12 +72,29 @@ def _http_get(url: str, fname: Union[str, Path]) -> NoReturn:
 
 def main() -> NoReturn:
     df = _fetch_final_results()
-    with pd.ExcelWriter(
-        BaseCfg.log_dir / "final_scores.xlsx", engine="openpyxl"
-    ) as writer:
-        for name, df_ in df.items():
-            df_.to_excel(writer, sheet_name=name, index=False)
-    print(f"final results saved to {str(BaseCfg.log_dir / 'final_scores.xlsx')}")
+    updated = False
+    save_path = BaseCfg.log_dir.parent / "results" / "final_results.xlsx"
+    df_old = (
+        pd.read_excel(save_path, sheet_name=None, engine="openpyxl")
+        if save_path.exists()
+        else None
+    )
+    if df_old is None:
+        updated = True
+    else:
+        for name in _URLS:
+            if not df_old[name].equals(df[name]):
+                updated = True
+                break
+    if updated:
+        with pd.ExcelWriter(save_path, engine="openpyxl") as writer:
+            for name, df_ in df.items():
+                df_.to_excel(writer, sheet_name=name, index=False)
+        print(f"final results saved to {str(save_path)}")
+    else:
+        mtime = datetime.fromtimestamp(save_path.stat().st_mtime)
+        mtime = datetime.strftime(mtime, "%Y-%m-%d %H:%M:%S")
+        print(f"final results is up-to-date, last updated at {mtime}")
 
 
 if __name__ == "__main__":
